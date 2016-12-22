2dc06302d2ecb568b944b3a4e0d2f554f5373666
jester <jestersimpps@gmail.com>
2016-12-22 08:32:26 +0100

    update 1

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"net/http"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
+ 
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", defaultHandler)
+    http.HandleFunc("/", handler)
     fmt.Println("Example app listening at http://localhost:8888")
     http.ListenAndServe(":8888", nil)
 }
