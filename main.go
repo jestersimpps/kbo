adce7e9e11c50a7784eabb53cb35c4b0f2cd7162
jester <jestersimpps@gmail.com>
2016-12-22 08:54:20 +0100

    echo

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"github.com/labstack/echo"
 )
 
- 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Example app listening at http://localhost:8888")
-    http.ListenAndServe(":8888", nil)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
